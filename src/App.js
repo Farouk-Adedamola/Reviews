3a22673b5b4f7f79bb92ce4ee2ffe2e6f2ba0090
unknown <faroukadedamola1@gmail.com>
2022-09-19 11:10:52 +0100

    initial commit
    A review card that only generate a user from a custom data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Reviews from "./Component/Reviews";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <section>
+        <header className="header">
+          <h1 className="title">reviews</h1>
+          <div className="underline"></div>
+        </header>
+      </section>
+      <main className="card">
+        <Reviews />
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Component/Reviews.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import reviews from "../data";
+import "./Reviews.css";
+
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Reviews = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = reviews[index];
+
+  const checkNumber = (number) => {
+    if (number > reviews.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return reviews.length - 1;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex(() => {
+      let newPerson = index + 1;
+
+      return checkNumber(newPerson);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex(() => {
+      let lastPerson = index - 1;
+      return checkNumber(lastPerson);
+    });
+  };
+
+  const randomPerson = () => {
+    let random = Math.floor(Math.random() * reviews.length);
+    if (random === index) {
+      random = index + 1;
+    }
+    return setIndex(checkNumber(random));
+  };
+  return (
+    <article>
+      <section className="section">
+        <div className="img-container">
+          <img src={image} alt="name" className="img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <div className="name-container">
+          <h3>{name}</h3>
+          <h4>{job}</h4>
+        </div>
+        <p>{text}</p>
+        <div className="btn">
+          <button className="left-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="right-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="random-btn" type="button" onClick={randomPerson}>
+          random user
+        </button>
+      </section>
+    </article>
+  );
+};
+
+export default Reviews;
